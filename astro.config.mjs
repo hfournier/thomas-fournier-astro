796d91e378bcc3c5fe0a7645eb0c6dfb9bef2710
Henri Fournier <hfournier@intratela.com>
2025-03-12 10:57:36 -0700

    updated to tailwind v4 and some cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,37 @@
 import { defineConfig } from "astro/config"
 import netlify from "@astrojs/netlify"
-import tailwind from "@astrojs/tailwind"
 
 import icon from "astro-icon"
+
+import tailwindcss from "@tailwindcss/vite"
 
 // https://astro.build/config
 export default defineConfig({
 	output: "server",
+
 	adapter: netlify({
 		cacheOnDemandPages: true
 	}),
+
+	image: {
+		// Used for all Markdown images; not configurable per-image
+		// Used for all `<Image />` and `<Picture />` components unless overridden with a prop
+		experimentalLayout: "none"
+	},
+	experimental: {
+		responsiveImages: true,
+		svg: true
+	},
+
 	integrations: [
-		tailwind({
-			applyBaseStyles: false
-		}),
 		icon({
 			include: {
 				mdi: ["close", "email", "phone", "linkedin", "twitter", "facebook", "close-circle"]
 			}
 		})
-	]
+	],
+
+	vite: {
+		plugins: [tailwindcss()]
+	}
 })
