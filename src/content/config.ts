86cd72ad1ad3777e62b718a6dee23758f3235241
Henri Fournier <hfournier@intratela.com>
2025-10-16 17:04:22 -0700

    code improvements

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,30 +1,37 @@
 import { defineCollection, z, type SchemaContext } from "astro:content"
 
+const baseGallerySchema = ({ image }: SchemaContext) =>
+    z.object({
+        image: image(),
+        role: z.string(),
+        play: z.string(),
+        author: z.string().optional(),
+        credit: z.string().optional(),
+        attribution: z.string().optional(),
+        sortOrder: z.number(),
+    })
+
 const gallerySchema = ({ image }: SchemaContext) =>
-	z.object({
-		image: image(),
-		role: z.string(),
-		play: z.string(),
-		author: z.string().optional(),
-		credit: z.string().optional(),
-		attribution: z.string().optional(),
-		sortOrder: z.number(),
-		type: z.string().default("image").optional(),
-		video: z
-			.object({
-				id: z.string(),
-				title: z.string().optional(),
-				startTime: z.string().optional(),
-				autoplay: z.boolean().default(false).optional()
-			})
-			.optional()
-	})
+    z.discriminatedUnion("type", [
+        baseGallerySchema({ image }).extend({
+            type: z.literal("image").optional().default("image"),
+        }),
+        baseGallerySchema({ image }).extend({
+            type: z.literal("video"),
+            video: z.object({
+                id: z.string(),
+                title: z.string().optional(),
+                startTime: z.number().optional(),
+                autoplay: z.boolean().default(false),
+            }),
+        }),
+    ])
 
 const galleryCollection = defineCollection({
-	type: "data",
-	schema: gallerySchema
+    type: "data",
+    schema: gallerySchema
 })
 
 export const collections = {
-	gallery: galleryCollection
+    gallery: galleryCollection
 }
